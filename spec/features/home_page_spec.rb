674a3b232d309ae1bc7bb5a95a6b666a22e77db6
Lanny Rivest <lanny.rivest@gmail.com>
2020-10-20 14:58:08 -0400

    Add product details spec

--- spec/features/home_page_spec.rb
+++ spec/features/home_page_spec.rb
@@ -20,8 +20,8 @@ RSpec.feature "Visitor navigates to home page", type: :feature, js: true do
       visit root_path
   
       # commented out b/c it's for debugging only
-      save_and_open_screenshot
+      # save_and_open_screenshot
   
-      expect(page).to have_css 'article.product', count: 10
+      expect(page).to have_css 'product-detail'
     end
 end
